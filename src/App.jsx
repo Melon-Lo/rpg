d0336e6bf2b774ecac0c12029feae0f57c26adff
Melon-Lo <dp98062@gmail.com>
2024-04-16 11:21:13 +0800

    feat/ add components: CreatePage & Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import CreatePage from "./pages/CreatePage";
+
 export default function App () {
   return (
-    <div className="mt-10">
-      app
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
     </div>
   );
 }
---
+++ src/pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+import Button from "../components/Button";
+
+export default function CreatePage() {
+  return (
+    <div>
+      <Button primary rounded>創建角色</Button>
+    </div>
+  );
+}
