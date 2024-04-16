da1156ae0f2faf1f58fddbd5b52d6d47f7d7883b
Melon-Lo <dp98062@gmail.com>
2024-04-16 11:34:51 +0800

    feat/ add some components & add navigation logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import CreatePage from "./pages/CreatePage";
+import LandingPage from "./pages/LandingPage";
+import Route from "./components/Route";
 
 export default function App () {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <div className="col-span-5">
+        <Route path='/landing'>
+          <LandingPage />
+        </Route>
+        <Route path='/create'>
+          <CreatePage />
+        </Route>
+      </div>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import './index.css';
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import { NavigationProvider } from './contexts/navigation';
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
 root.render(
-  <App />
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
 )
