ffd9a1fd76a2a92f002f03825b9548e32f7746c4
Melon-Lo <dp98062@gmail.com>
2024-04-16 10:42:58 +0800

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+import React from 'react';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = createRoot(el);
+
+root.render(
+  <App />
+)
