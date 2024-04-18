8a38e8f2350a77d34bce096ebec5cc4fb36edc06
Melon-Lo <dp98062@gmail.com>
2024-04-18 16:56:55 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,25 @@ import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import CreatePage from "./pages/CreatePage";
 import LandingPage from "./pages/LandingPage";
-import Route from "./components/Route";
+// import Route from "./components/Route";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const basename = process.env.PUBLIC_URL;
 
 export default function App () {
   return (
-    <div className="">
+    <div className="app">
       <Header />
-      <div className="mt-20">
-        <Route path='/'>
-          <MainPage />
-        </Route>
-        <Route path='/landing'>
-          <LandingPage />
-        </Route>
-        <Route path='/create'>
-          <CreatePage />
-        </Route>
-      </div>
+      <BrowserRouter basename={basename}>
+        <div className="mt-20">
+          <Routes>
+            <Route path='/' element={<MainPage />}></Route>
+            <Route path='create' element={<CreatePage />}></Route>
+            <Route path='landing' element={<LandingPage />}></Route>
+            <Route path='*' element={<CreatePage />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
