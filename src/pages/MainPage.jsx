7d1bab9bc860895357df97dcd835f0a148808983
Melon-Lo <dp98062@gmail.com>
2024-04-18 18:19:08 +0800

    test

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,13 @@
-import StatusSection from "../components/StatusSection"
+import StatusSection from "../components/StatusSection";
+import { useNavigate } from "react-router-dom";
 
 export default function MainPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center">
       <StatusSection />
+      <button onClick={() => navigate('create')}>to create</button>
     </div>
-  )
+  );
 };
