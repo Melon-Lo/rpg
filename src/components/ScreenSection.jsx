4cd82bea801cb0bce0b4e459054900fc445506ce
Melon-Lo <dp98062@gmail.com>
2024-04-19 14:19:36 +0800

    feat/ update ScreenSection

--- src/components/ScreenSection.jsx
+++ src/components/ScreenSection.jsx
@@ -11,7 +11,7 @@ export default function ScreenSection() {
 
   return (
     <section className="relative w-full h-72 my-5 flex justify-center">
-      <div className="relative w-11/12 h-full border-4 border-gray-800 rounded-md">
+      <div className="relative w-11/12 h-full border-2 border-gray-600 rounded-md overflow-hidden">
         <h1 className="absolute top-3 right-3 z-10 bg-slate-100/70 py-1 px-2 rounded text-gray-800">當前地點：{currentLocation}</h1>
         <img src={currentLocationImgSrc} alt="scene-img" className="absolute w-full h-full object-cover" />
       </div>
