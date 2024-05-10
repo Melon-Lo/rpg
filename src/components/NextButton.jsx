dcf073d0369693cd8b05d45713e44a3d3618d0e7
Melon-Lo <dp98062@gmail.com>
2024-05-10 18:10:14 +0800

    feat/ update quest system

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -15,17 +15,18 @@ export default function NextButton({ sentence, setSentence }) {
   const questDialogues = quests.find(quest => quest.npc === talker)?.dialogues;
 
   const handleClick = () => {
+    // 每一句對話
     dispatch(addMessage({
       type: 'basic',
       content: `${talker}：「${content[sentence]}」`,
     }))
 
-    // 每講一句，就推進一句
-    setSentence(sentence + 1);
-
-    // 如果沒任務，講到最後一句時，自動退回主頁
+    // 如果沒任務
     if (!questDialogues) {
-      if (sentence === contentLength - 1) {
+      // 每講一句，就推進一句
+      if (sentence < contentLength - 1) {
+        setSentence(sentence + 1);
+      } else {
         setCurrentStep('主頁');
         setSentence(0);
       }
@@ -33,27 +34,26 @@ export default function NextButton({ sentence, setSentence }) {
 
     // 如果有任務，根據情況給不同的對話
     if (questDialogues) {
-      
+      if (sentence < contentLength - 1) {
+        setSentence(sentence + 1);
+      } else {
+        setCurrentStep('主頁');
+        setSentence(0);
+      }
+      // Swal.fire({
+      //   title: '要幫忙嗎？',
+      //   icon: 'question',
+      //   showCancelButton: true,
+      //   confirmButtonColor: '#3085d6',
+      //   cancelButtonColor: '#d33',
+      //   confirmButtonText: '幫忙',
+      //   cancelButtonText: '算了'
+      // }).then((result) => {
+      //   if (result.isConfirmed) {
+      //     console.log('yes')
+      //   }
+      // });
     }
-
-    // if (questDialogues) {
-    //   Swal.fire({
-    //     title: '要幫忙嗎？',
-    //     icon: 'question',
-    //     showCancelButton: true,
-    //     confirmButtonColor: '#3085d6',
-    //     cancelButtonColor: '#d33',
-    //     confirmButtonText: '幫忙',
-    //     cancelButtonText: '算了'
-    //   }).then((result) => {
-    //     if (result.isConfirmed) {
-    //       console.log('yes')
-    //     }
-    //   });
-    // } else {
-    //   setCurrentStep('主頁');
-    //   setSentence(0);
-    // }
   }
 
   return (
