ca151773ac86549fc55aee375e50b3711945aed9
Melon-Lo <dp98062@gmail.com>
2024-05-10 14:42:35 +0800

    feat/ refactor ShopModal

--- src/components/ShopModal.jsx
+++ src/components/ShopModal.jsx
@@ -13,7 +13,7 @@ export default function ShopModal() {
   const dispatch = useDispatch();
   const { setShowModal } = useContext(ModalContext);
   const { setCurrentStep } = useContext(StepContext);
-  const { currentScene } = useSelector(state => state.systemStatus);
+  const { currentScene, stage } = useSelector(state => state.systemStatus);
   const { money, data: items } = useSelector(state => state.items);
   const currentShopItems = shopsItems.find(shop => shop.shop === currentScene).items;
 
@@ -108,79 +108,102 @@ export default function ShopModal() {
     });
   };
 
-  const currentItems = type === 'buy' ? currentShopItems : items;
-  const renderedItems = currentItems.map(item => {
+  // 購買
+  const renderedBuyItems = currentShopItems.map(item => {
     const itemName = type === 'buy' ? item.item : item.name;
-    // 賣價為買價的 50% 
-    const sellItemPrice = Math.floor(shopsItems.find(shop => shop.shop === currentScene).items.find(item => item.item === itemName).price) / 2;
-    const itemPrice = type === 'buy' ? item.price : sellItemPrice;
     const currentHeld = items.find(item => item.name === itemName)?.quantity || 0;
-    const itemQuantityHeld = type === 'buy' ? currentHeld : item.quantity;
+
+    // 如果還沒到達特定 stage，不會販賣該品項
+    if (stage < item.stage) return null;
 
     return (
       <ShopItem
-        key={itemName}
-        item={itemName}
-        price={itemPrice}
+        key={item.item}
+        item={item.item}
+        price={item.price}
         total={total}
         setTotal={setTotal}
         shoppingCart={shoppingCart}
         setShoppingCart={setShoppingCart}
         type={type}
-        quantityHeld={itemQuantityHeld}
+        quantityHeld={currentHeld}
+      />
+    )
+  })
+
+  // 販賣
+  const renderedSellItems = items.map(item => {
+    const itemName = item.name;
+    // 賣價為買價的 50%
+    const sellItemPrice = Math.floor(shopsItems.find(shop => shop.shop === currentScene).items.find(item => item.item === itemName).price) / 2;
+
+    return (
+      <ShopItem
+        key={itemName}
+        item={itemName}
+        price={sellItemPrice}
+        total={total}
+        setTotal={setTotal}
+        shoppingCart={shoppingCart}
+        setShoppingCart={setShoppingCart}
+        type={type}
+        quantityHeld={item.quantity}
       />
     )
   })
 
   return (
-      <div className="fixed bg-gray-800/75 inset-0 z-10">
-        <div className="relative w-full h-full">
-          <div className="absolute top-20 left-1/2 -translate-x-1/2 z-20 bg-amber-200 w-11/12 h-5/6 max-w-[768px] rounded-lg flex flex-col justify-between">
-            <div className="relative flex flex-col justify-center items-center py-2">
-              <h1 className="text-3xl text-amber-900">商店</h1>
-              <h2 className="text-amber-800 py-2">＊點擊商品可以查看效果</h2>
-              <div className="absolute top-2 right-2" onClick={handleCloseModal}>
-                <RxCross1 className="text-2xl text-amber-900" />
-              </div>
+    <div className="fixed bg-gray-800/75 inset-0 z-10">
+      <div className="relative w-full h-full">
+        <div className="absolute top-20 left-1/2 -translate-x-1/2 z-20 bg-amber-200 w-11/12 h-5/6 max-w-[768px] rounded-lg flex flex-col justify-between">
+          <div className="relative flex flex-col justify-center items-center py-2">
+            <h1 className="text-3xl text-amber-900">商店</h1>
+            <h2 className="text-amber-800 py-2">＊點擊商品可以查看效果</h2>
+            <div className="absolute top-2 right-2" onClick={handleCloseModal}>
+              <RxCross1 className="text-2xl text-amber-900" />
             </div>
-            <div className="flex justify-center items-center pb-5 text-lg">
-              <div
-                className={typeStyle + (type === 'buy' && selectedStyle)}
-                onClick={handleSetTypeBuy}
-              >
-                購買
-              </div>
-              <div
-                className={typeStyle + (type === 'sell' && selectedStyle)}
-                onClick={handleSetTypeSell}
-              >
-                販賣
-              </div>
+          </div>
+          <div className="flex justify-center items-center pb-5 text-lg">
+            <div
+              className={typeStyle + (type === 'buy' && selectedStyle)}
+              onClick={handleSetTypeBuy}
+            >
+              購買
             </div>
-            <div className="h-[500px] bg-slate-100/75 overflow-y-scroll">
-              { renderedItems.length > 0 ?
-                renderedItems :
+            <div
+              className={typeStyle + (type === 'sell' && selectedStyle)}
+              onClick={handleSetTypeSell}
+            >
+              販賣
+            </div>
+          </div>
+          <div className="h-[500px] bg-slate-100/75 overflow-y-scroll">
+            { type === 'buy' && renderedBuyItems }
+            { type === 'sell' &&
+              ( renderedSellItems.length > 0 ?
+                renderedSellItems :
                 <p className="p-3 text-lg text-center">
                   未持有任何物品
                 </p>
-              }
+              )
+            }
+          </div>
+          <div className="flex justify-between items-center px-3 py-5">
+            <div>
+              <h5 className="text-xs text-gray-600">持有金錢：$ {money}</h5>
+              <h5 className="text-xl text-gray-800">總計：$ {total}</h5>
             </div>
-            <div className="flex justify-between items-center px-3 py-5">
-              <div>
-                <h5 className="text-xs text-gray-600">持有金錢：$ {money}</h5>
-                <h5 className="text-xl text-gray-800">總計：$ {total}</h5>
-              </div>
-              {/* 根據不同條件渲染 submit 按鈕 */}
-              <Button
-                onClick={handleSubmit}
-                disabled={(type === 'buy' && (money < total || total === 0)) || (type === 'sell' && total === 0)}
-                amber={(type === 'buy' && money >= total && total !== 0) || (type === 'sell' && total !== 0)}
-              >
-                確定{typeText}
-              </Button>
-            </div>
+            {/* 根據不同條件渲染 submit 按鈕 */}
+            <Button
+              onClick={handleSubmit}
+              disabled={(type === 'buy' && (money < total || total === 0)) || (type === 'sell' && total === 0)}
+              amber={(type === 'buy' && money >= total && total !== 0) || (type === 'sell' && total !== 0)}
+            >
+              確定{typeText}
+            </Button>
           </div>
         </div>
       </div>
+    </div>
   )
 }
--- src/data/shopsItems.js
+++ src/data/shopsItems.js
@@ -4,34 +4,42 @@ const shopsItems = [
     items: [
       {
         item: "補藥",
+        stage: 1,
         price: 20,
       },
       {
         item: "厲害補藥",
+        stage: 2,
         price: 50,
       },
       {
         item: "強效補藥",
+        stage: 3,
         price: 100,
       },
       {
         item: "究極補藥",
+        stage: 4,
         price: 300,
       },
       {
         item: "魔法藥",
+        stage: 1,
         price: 50,
       },
       {
         item: "高等魔法藥",
+        stage: 2,
         price: 80,
       },
       {
         item: "大師魔法藥",
+        stage: 3,
         price: 200,
       },
       {
         item: "女神之吻",
+        stage: 5,
         price: 1000,
       },
     ],
